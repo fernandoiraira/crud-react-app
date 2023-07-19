c7c4774c7b3e01c193bce9351a87f78750d0ccf9
fernandoiraira <fmiraira@gmail.com>
2023-07-19 10:34:27 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const mysql = require("./mysql_connector");
 
 app.use(express.json());
+app.use(cors());
+
+app.post("/create/empleado", (req, res) => {
+  const nombre = req.body.nombre;
+  const edad = req.body.edad;
+  const pais = req.body.pais;
+  const cargo = req.body.cargo;
+  const antiguedad = req.body.antiguedad;
+});
 
 app.listen(port, (req, res) => {
   console.log("Servidor corriendo en el puerto " + port);
