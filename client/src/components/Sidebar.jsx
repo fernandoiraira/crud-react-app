b2c234aa4f5d202b3dd74065e13d35a019e6d873
fernandoiraira <fmiraira@gmail.com>
2023-07-19 12:15:52 -0300

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -11,6 +11,18 @@ function Sidebar() {
               <h2 className="text-white mx-4 text-decoration-none">BRAND</h2>
             </Link>
           </div>
+
+          <ul>
+            <i>
+              <Link to="/productos" className="nav-link px-2 text-white">
+                <i className="bi-box-seam">
+                  <span className="ms-3 d-none d-sm-inline text-white">
+                    Productos
+                  </span>
+                </i>
+              </Link>
+            </i>
+          </ul>
         </div>
       </div>
     </div>
