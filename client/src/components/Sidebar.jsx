792b891f496ba1ff463efbd8837971c311100bdb
fernandoiraira <fmiraira@gmail.com>
2023-07-19 11:19:50 -0300

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,3 +1,8 @@
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import "./Sidebar.css";
+import { Link } from "react-router-dom";
+
 function Sidebar() {
   return (
     <div>Sidebar</div>
