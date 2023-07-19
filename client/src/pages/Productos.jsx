bb923ff04ba377203bf3f48c075c4942cd1fde73
fernandoiraira <fmiraira@gmail.com>
2023-07-19 15:56:22 -0300

    Update Productos.jsx

--- client/src/pages/Productos.jsx
+++ client/src/pages/Productos.jsx
@@ -3,7 +3,118 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Productos() {
-  return <h1>ESTA ES LA PAGINA DE PRODUCTOS</h1>;
+  const [formData, setFormData] = useState({
+    id: null,
+    nombre: "",
+    precioCosto: null,
+    precioVenta: null,
+    stock: null,
+    descripcion: "",
+    fechaCompra: null,
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="container">
+      <div className="card text-center mt-5">
+        <div className="card-header">GESTIÓN DE PRODUCTOS</div>
+        <div className="card-body">
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">
+                Nombre:
+              </span>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Ingrese el nombre del producto"
+              aria-label="Nombre"
+              aria-describedby="basic-addon1"
+              onChange={handleChange}
+              name="nombre"
+              value={formData.nombre}
+            />
+          </div>
+
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">
+                Precio costo:
+              </span>
+            </div>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Ingrese el precio costo"
+              aria-label="PrecioCosto"
+              aria-describedby="basic-addon1"
+              onChange={handleChange}
+              name="precioCosto"
+              value={formData.precioCosto}
+            />
+          </div>
+
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">
+                Precio venta:
+              </span>
+            </div>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Ingrese el precio de venta"
+              aria-label="PrecioVenta"
+              aria-describedby="basic-addon1"
+              onChange={handleChange}
+              name="precioVenta"
+              value={formData.precioVenta}
+            />
+          </div>
+
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">
+                Stock:
+              </span>
+            </div>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Ingrese el stock del producto"
+              aria-label="Stock"
+              aria-describedby="basic-addon1"
+              onChange={handleChange}
+              name="stock"
+              value={formData.stock}
+            />
+          </div>
+
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">
+                Descripción:
+              </span>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Ingrese una descripcion del producto"
+              aria-label="Descripcion"
+              aria-describedby="basic-addon1"
+              onChange={handleChange}
+              name="descripcion"
+              value={formData.descripcion}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Productos;
