14438ba62571c701c2a6d90ad8f85a4e025ca011
fernandoiraira <fmiraira@gmail.com>
2023-07-19 11:40:53 -0300

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <Router>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-2 px-0">
+            <Sidebar />
+          </div>
+          <div className="col-10 px-0"></div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
