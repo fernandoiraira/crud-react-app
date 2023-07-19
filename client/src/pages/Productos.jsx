4bf28269fe833052b381d865b605ff02debd3913
fernandoiraira <fmiraira@gmail.com>
2023-07-19 15:35:05 -0300

    Update Productos.jsx
    Se realizaron los imports necesarios

--- client/src/pages/Productos.jsx
+++ client/src/pages/Productos.jsx
@@ -1,3 +1,7 @@
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Productos() {
   return <h1>ESTA ES LA PAGINA DE PRODUCTOS</h1>;
 }
