8278d19b7723ce59bad3b01c58dff7654303475c
fernandoiraira <fmiraira@gmail.com>
2023-07-19 17:51:11 -0300

    Update GetProductos.jsx

--- client/src/pages/GetProductos.jsx
+++ client/src/pages/GetProductos.jsx
@@ -1,5 +1,69 @@
-function GetProductos() {
-  return <h1>ESTA ES LA PAGINA DE GETPRODUCTOS</h1>;
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Dropdown from "react-bootstrap/Dropdown";
+import { Link } from "react-router-dom";
+
+function Productos() {
+  const [formData, setFormData] = useState({
+    id: null,
+    nombre: "",
+    precioCosto: null,
+    precioVenta: null,
+    stock: null,
+    descripcion: "",
+    fechaCompra: null,
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const add = () => {
+    axios
+      .post("http://localhost:3001/create/producto", {
+        nombre: formData.nombre,
+        precioCosto: formData.precioCosto,
+        precioVenta: formData.precioVenta,
+        stock: formData.stock,
+        descripcion: formData.descripcion,
+      })
+      .then(() => {
+        alert("Producto registrado!");
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="container">
+        <Dropdown>
+          <Dropdown.Toggle variant="primary" id="dropdown-basic">
+            Acciones
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item as={Link} to="/productos">
+              Cargar nuevo producto
+            </Dropdown.Item>
+            <Dropdown.Item as={Link} to="/get/productos">
+              Todos los productos
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <div className="card text-center mt-5">
+        <div className="card-header">VER PRODUCTOS</div>
+        <div className="card-body">
+          
+        </div>
+        <div className="card-footer text-muted">
+          <button onClick={add} className="btn btn-success mx-2">
+            Registrar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default GetProductos;
+export default Productos;
