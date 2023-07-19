d6d43771e1f5a3d43dd210c1bea8b38a88e56d58
fernandoiraira <fmiraira@gmail.com>
2023-07-19 10:30:10 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
 app.listen(port, (req, res) => {
   console.log("Servidor corriendo en el puerto " + port);
 });
