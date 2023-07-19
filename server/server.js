4f9a7cbcb1060eeddf9364deb169f2eebb2f3ef5
fernandoiraira <fmiraira@gmail.com>
2023-07-19 10:38:22 -0300

    Update server.js
    Se agrego funcionalidad para registrar empleado

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-const mysql = require("./mysql_connector");
+const db = require("./mysql_connector");
 
 app.use(express.json());
 app.use(cors());
@@ -12,6 +12,15 @@ app.post("/create/empleado", (req, res) => {
   const pais = req.body.pais;
   const cargo = req.body.cargo;
   const antiguedad = req.body.antiguedad;
+
+  db.query(
+    "INSERT INTO empleados (nombre,edad,pais,cargo,antiguedad) VALUES (?,?,?,?,?)",
+    [nombre, edad, pais, cargo, antiguedad],
+    (err, result) => {
+      if (err) throw err;
+      res.send("Empleado registrado con exito");
+    }
+  );
 });
 
 app.listen(port, (req, res) => {
