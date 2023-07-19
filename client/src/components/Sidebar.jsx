f0eff2bd5912ae0a2c685d0db385f6d17e871548
fernandoiraira <fmiraira@gmail.com>
2023-07-19 11:21:26 -0300

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,8 +5,18 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="container-fluid">
+      <div className="row">
+        <div className="min-vh-100 bg-dark">
+          <div className="my-3">
+            <Link to="/" className="text-decoration-none">
+              <h2 className="text-white mx-4 text-decoration-none">BRAND</h2>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
