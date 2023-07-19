a9cac5b7ab7025fd160af195f982deb47a119ba0
fernandoiraira <fmiraira@gmail.com>
2023-07-19 10:29:38 -0300

    Update mysql_connector.js

--- server/mysql_connector.js
+++ server/mysql_connector.js
@@ -1,3 +1,9 @@
 const mysql = require("mysql");
+const user_data = require("./user_data");
 
-const db = mysql.createConnection({});
+const db = mysql.createConnection({
+  host: user_data.host,
+  user: user_data.user,
+  password: user_data.password,
+  database: user_data.database,
+});
