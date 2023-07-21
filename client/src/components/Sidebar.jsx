9469c8a0147bb0433b1389a311ead759388012af
fernandoiraira <fmiraira@gmail.com>
2023-07-21 12:39:07 -0300

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import { Sidenav, Nav } from "rsuite";
 import { useState } from "react";
 import DashboardIcon from "@rsuite/icons/legacy/Dashboard";
-import GroupIcon from "@rsuite/icons/legacy/Group";
 import MagicIcon from "@rsuite/icons/legacy/Magic";
 import GearCircleIcon from "@rsuite/icons/legacy/GearCircle";
 import "rsuite/dist/rsuite.min.css";
+import TrendIcon from "@rsuite/icons/Trend";
 import { Link } from "react-router-dom";
 
 function Sidebar() {
@@ -17,14 +17,6 @@ function Sidebar() {
           <Nav activeKey={activeKey} onSelect={setActiveKey}>
             <Nav.Item eventKey="1" icon={<DashboardIcon />} as={Link} to="/">
               GESTIÓN
-            </Nav.Item>
-            <Nav.Item
-              eventKey="2"
-              icon={<GroupIcon />}
-              as={Link}
-              to="/clientes"
-            >
-              Clientes
             </Nav.Item>
             <Nav.Menu
               placement="rightStart"
@@ -42,16 +34,24 @@ function Sidebar() {
             <Nav.Menu
               placement="rightStart"
               eventKey="4"
-              title="Settings"
+              title="Clientes"
               icon={<GearCircleIcon />}
             >
-              <Nav.Item eventKey="4-1">Applications</Nav.Item>
-              <Nav.Item eventKey="4-2">Channels</Nav.Item>
-              <Nav.Item eventKey="4-3">Versions</Nav.Item>
-              <Nav.Menu eventKey="4-5" title="Custom Action">
+              <Nav.Item eventKey="4-1">Cargar Cliente</Nav.Item>
+              <Nav.Item eventKey="4-2">Consultar Clientes</Nav.Item>
+              {/* <Nav.Menu eventKey="4-5" title="Custom Action">
                 <Nav.Item eventKey="4-5-1">Action Name</Nav.Item>
                 <Nav.Item eventKey="4-5-2">Action Params</Nav.Item>
-              </Nav.Menu>
+              </Nav.Menu> */}
+            </Nav.Menu>
+            <Nav.Menu
+              placement="rightStart"
+              eventKey="5"
+              title="Ventas"
+              icon={<TrendIcon />}
+            >
+              <Nav.Item eventKey="5-1">Cargar Venta</Nav.Item>
+              <Nav.Item eventKey="5-2">Consultar Ventas</Nav.Item>
             </Nav.Menu>
           </Nav>
         </Sidenav.Body>
