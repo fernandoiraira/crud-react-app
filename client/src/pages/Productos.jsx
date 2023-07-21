c28a5b5c055384c5faae88fc48d9cfd840da8459
fernandoiraira <fmiraira@gmail.com>
2023-07-21 11:09:03 -0300

    Update Productos.jsx
    Se eliminan imports innecesarios

--- client/src/pages/Productos.jsx
+++ client/src/pages/Productos.jsx
@@ -1,5 +1,5 @@
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Dropdown from "react-bootstrap/Dropdown";
 import { Link } from "react-router-dom";
