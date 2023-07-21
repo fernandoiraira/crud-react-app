024ce4e7bc623142c9916b476ed5485e9afdef80
fernandoiraira <fmiraira@gmail.com>
2023-07-21 12:18:13 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,14 @@ import Productos from "./pages/Productos";
 import Clientes from "./pages/Clientes";
 import Proveedores from "./pages/Proveedores";
 import GetProductos from "./pages/GetProductos";
+import "rsuite/dist/rsuite.min.css";
 
 function App() {
   return (
     <Router>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-2 px-0">
+      <div className="container-fluid vh-100">
+        <div className="row vh-100">
+          <div className="col-2 px-0 vh-100">
             <Sidebar />
           </div>
           <div className="col-10 px-0">
