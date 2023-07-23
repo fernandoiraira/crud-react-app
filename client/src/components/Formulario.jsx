3b6a6642e96fd09ec5bf144a65abe3e351c0f937
fernandoiraira <fmiraira@gmail.com>
2023-07-23 11:22:11 -0300

    Update Formulario.jsx

---
+++ client/src/components/Formulario.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function Formulario() {
+  return <div>Formulario</div>;
+}
+
+export default Formulario;
