53226dea332c1a6e472d8257f4809fcf12e5e062
fernandoiraira <fmiraira@gmail.com>
2023-07-23 13:35:08 -0300

    Update ErrorPage.jsx
    Se agregan datos de testeo

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,10 +1,21 @@
 import Buscador from "../components/Buscador";
 
 export default function ErrorPage() {
+  const dataEncabezado = [
+    "ID",
+    "Nombre",
+    "Precio Costo",
+    "Precio Venta",
+    "Stock",
+    "Descripcion",
+    "Fecha de Compra",
+  ];
+
   return (
     <Buscador
       titulo="LISTA DE PRODUCTOS TEST"
       placeholder="Ingrese el nombre del producto test"
+      encabezado={dataEncabezado}
     />
   );
 }
