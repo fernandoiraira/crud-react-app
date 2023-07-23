32483a752c5c725c36a0f854b6240b2ebe8b5acd
fernandoiraira <fmiraira@gmail.com>
2023-07-23 13:50:47 -0300

    Update Productos.jsx
    Se modiifico el codigo para usar el componente Formulario

--- client/src/pages/Productos.jsx
+++ client/src/pages/Productos.jsx
@@ -45,107 +45,53 @@ function Productos() {
       });
   };
 
+  const atributos = [
+    {
+      label: "Nombre",
+      placeholder: "Ingrese el nombre del producto",
+      name: "nombre",
+      type: "text",
+    },
+    {
+      label: "Precio Costo",
+      placeholder: "Ingrese el precio costo del producto",
+      name: "precioCosto",
+      type: "number",
+    },
+    {
+      label: "Precio Venta",
+      placeholder: "Ingrese el precio venta del producto",
+      name: "precioVenta",
+      type: "number",
+    },
+    {
+      label: "Stock",
+      placeholder: "Ingrese el stock del producto",
+      name: "stock",
+      type: "number",
+    },
+    {
+      label: "Descripcion",
+      placeholder: "Ingrese la descripcion del producto",
+      name: "descripcion",
+      type: "text",
+    },
+    {
+      label: "Fecha Compra",
+      placeholder: "Ingrese la fecha de compra del producto",
+      name: "fechaCompra",
+      type: "date",
+    },
+  ];
+
   return (
     <div className="container">
-      <div className="card text-center mt-5">
-        <div className="card-header">CARGA DE PRODUCTOS</div>
-        <div className="card-body">
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                Nombre:
-              </span>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Ingrese el nombre del producto"
-              aria-label="Nombre"
-              aria-describedby="basic-addon1"
-              onChange={handleChange}
-              name="nombre"
-              value={formData.nombre}
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                Precio costo:
-              </span>
-            </div>
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Ingrese el precio costo"
-              aria-label="PrecioCosto"
-              aria-describedby="basic-addon1"
-              onChange={handleChange}
-              name="precioCosto"
-              value={formData.precioCosto}
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                Precio venta:
-              </span>
-            </div>
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Ingrese el precio de venta"
-              aria-label="PrecioVenta"
-              aria-describedby="basic-addon1"
-              onChange={handleChange}
-              name="precioVenta"
-              value={formData.precioVenta}
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                Stock:
-              </span>
-            </div>
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Ingrese el stock del producto"
-              aria-label="Stock"
-              aria-describedby="basic-addon1"
-              onChange={handleChange}
-              name="stock"
-              value={formData.stock}
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                Descripción:
-              </span>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Ingrese una descripcion del producto"
-              aria-label="Descripcion"
-              aria-describedby="basic-addon1"
-              onChange={handleChange}
-              name="descripcion"
-              value={formData.descripcion}
-            />
-          </div>
-        </div>
-        <div className="card-footer text-muted">
-          <button onClick={add} className="btn btn-success mx-2">
-            Registrar
-          </button>
-        </div>
-      </div>
+      <Formulario
+        titulo="CARGA DE PRODUCTOS"
+        atributos={atributos}
+        handleChange={handleChange}
+        handleAdd={add}
+      />
     </div>
   );
 }
