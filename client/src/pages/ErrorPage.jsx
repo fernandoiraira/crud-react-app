1d2d3a9c0b97f9343fe5ce26aa7d650cb0130d35
fernandoiraira <fmiraira@gmail.com>
2023-07-23 17:58:38 -0300

    Update ErrorPage.jsx
    se crean funciones de handle

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,6 +1,11 @@
 import Buscador from "../components/Buscador";
+import axios from "axios";
 
 export default function ErrorPage() {
+  const getPath = "http://localhost:3001/get/clientes";
+  const delPath = "http://localhost:3001/delete/cliente";
+  const updatePath = "http://localhost:3001/update/cliente";
+
   const dataEncabezado = [
     "ID",
     "Nombre",
@@ -11,11 +16,19 @@ export default function ErrorPage() {
     "Fecha de Compra",
   ];
 
+  const handleGet = () => {};
+
+  const handleUpdate = () => {};
+  const handleDelete = () => {};
+
   return (
     <Buscador
       titulo="LISTA DE PRODUCTOS TEST"
       placeholder="Ingrese el nombre del producto test"
       encabezado={dataEncabezado}
+      handleGet={handleGet}
+      handleUpdate={handleUpdate}
+      handleDelete={handleDelete}
     />
   );
 }
