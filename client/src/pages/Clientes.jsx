471d6cf610011ba6a09d0ad25fda02c6ee06e7e1
fernandoiraira <fmiraira@gmail.com>
2023-07-23 11:46:19 -0300

    Update Clientes.jsx
    Se inicializa la pagina de clientes

--- client/src/pages/Clientes.jsx
+++ client/src/pages/Clientes.jsx
@@ -1,5 +1,53 @@
+import Formulario from "../components/Formulario";
+
 function Clientes() {
-  return <h1>ESTA ES LA PAGINA DE CLIENTES</h1>;
+  const dataFormulario = [
+    {
+      label: "Nombre:",
+      placeholder: "Ingrese el nombre del cliente",
+      name: "nombre",
+      type: "text",
+    },
+    {
+      label: "Apellido:",
+      placeholder: "Ingrese el apellido del cliente",
+      name: "apellido",
+      type: "text",
+    },
+    {
+      label: "Fecha de nacimiento",
+      placeholder: "Ingrese la fecha de nacimiento",
+      name: "fechaNacimiento",
+      type: "date",
+    },
+    {
+      label: "Email",
+      placeholder: "Ingrese el mail del cliente",
+      name: "email",
+      type: "email",
+    },
+    {
+      label: "Telefono",
+      placeholder: "Ingrese el telefono del cliente",
+      name: "telefono",
+      type: "tel",
+    },
+    {
+      label: "Fecha de registro",
+      placeholder: "Ingrese la fecha de registro",
+      name: "fechaRegistro",
+      type: "date",
+    },
+  ];
+
+  return (
+    <Formulario
+      titulo="CARGA DE CLIENTES"
+      atributos={dataFormulario}
+      handleChange={null}
+      handleAdd={null}
+    />
+  );
 }
 
 export default Clientes;
