f93b1f63b970422f42207f7ababfaf8466cfdc65
fernandoiraira <fmiraira@gmail.com>
2023-07-23 11:44:13 -0300

    Update ErrorPage.jsx
    Se modificaron los valores para testeo de formulario de la clase Clientes

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -4,35 +4,41 @@ export default function ErrorPage() {
   const dataFormulario = [
     {
       label: "Nombre:",
-      placeholder: "Ingrese el nombre del producto",
+      placeholder: "Ingrese el nombre del cliente",
       name: "nombre",
       type: "text",
     },
     {
-      label: "Precio costo:",
-      placeholder: "Ingrese el precio costo",
-      name: "precioCosto",
-      type: "number",
-    },
-    {
-      label: "Precio venta:",
-      placeholder: "Ingrese el precio venta",
-      name: "precioVenta",
-      type: "number",
-    },
-    {
-      label: "Stock",
-      placeholder: "Ingrese el stock del producto",
-      name: "stock",
-      type: "number",
-    },
-    {
-      label: "Descripcion",
-      placeholder: "Ingrese una descripcion del producto",
-      name: "descripcion",
+      label: "Apellido:",
+      placeholder: "Ingrese el apellido del cliente",
+      name: "apellido",
       type: "text",
+    },
+    {
+      label: "Fecha de nacimiento",
+      placeholder: "Ingrese la fecha de nacimiento",
+      name: "fechaNacimiento",
+      type: "date",
+    },
+    {
+      label: "Email",
+      placeholder: "Ingrese el mail del cliente",
+      name: "email",
+      type: "email",
+    },
+    {
+      label: "Telefono",
+      placeholder: "Ingrese el telefono del cliente",
+      name: "telefono",
+      type: "tel",
+    },
+    {
+      label: "Fecha de registro",
+      placeholder: "Ingrese la fecha de registro",
+      name: "fechaRegistro",
+      type: "date",
     },
   ];
 
-  return <Formulario />;
+  return <Formulario titulo="CARGA DE CLIENTES" atributos={dataFormulario} />;
 }
