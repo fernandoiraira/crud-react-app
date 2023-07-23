d5a27907d5e4167a50cfc2fe4153804b5f6dc74f
fernandoiraira <fmiraira@gmail.com>
2023-07-23 11:34:22 -0300

    Update ErrorPage.jsx
    Se genero objeto de informacion

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,5 +1,38 @@
 import Formulario from "../components/Formulario";
 
 export default function ErrorPage() {
+  const dataFormulario = [
+    {
+      label: "Nombre:",
+      placeholder: "Ingrese el nombre del producto",
+      name: "nombre",
+      type: "text",
+    },
+    {
+      label: "Precio costo:",
+      placeholder: "Ingrese el precio costo",
+      name: "precioCosto",
+      type: "number",
+    },
+    {
+      label: "Precio venta:",
+      placeholder: "Ingrese el precio venta",
+      name: "precioVenta",
+      type: "number",
+    },
+    {
+      label: "Stock",
+      placeholder: "Ingrese el stock del producto",
+      name: "stock",
+      type: "number",
+    },
+    {
+      label: "Descripcion",
+      placeholder: "Ingrese una descripcion del producto",
+      name: "descripcion",
+      type: "text",
+    },
+  ];
+
   return <Formulario />;
 }
