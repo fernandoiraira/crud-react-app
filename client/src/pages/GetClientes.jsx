6d21d715bf1b02a447a87bcf020c4e71ac0df083
fernandoiraira <fmiraira@gmail.com>
2023-07-24 08:46:52 -0300

    Update GetClientes.jsx
    Se agrego codigo para la utilizacion de Formulario

--- client/src/pages/GetClientes.jsx
+++ client/src/pages/GetClientes.jsx
@@ -1,8 +1,29 @@
+import Buscador from "../components/Buscador";
 
 function GetClientes() {
-  return (
-    <div>GetClientes</div>
-  )
-}
+  const getPath = "http://localhost:3001/get/clientes";
+  const delPath = "http://localhost:3001/delete/cliente";
+  const updatePath = "http://localhost:3001/update/cliente";
 
-export default GetClientes
+  const dataEncabezado = [
+    "ID",
+    "Nombre",
+    "Apellido",
+    "Fecha Nacimiento",
+    "Email",
+    "Teléfono",
+    "Fecha Registro",
+  ];
+
+  return (
+    <Buscador
+      titulo="BUSCAR CLIENTES"
+      placeholder="Ingrese el nombre del cliente"
+      getPath={getPath}
+      delPath={delPath}
+      updatePath={updatePath}
+      encabezado={dataEncabezado}
+    />
+  );
+}
+export default GetClientes;
