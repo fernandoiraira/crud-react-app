25896e485002cf118eeffa08f56599d020c827bd
fernandoiraira <fmiraira@gmail.com>
2023-07-27 07:12:34 -0300

    Update ErrorPage.jsx
    Se modifico la pagina de error

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,28 +1,3 @@
-import Buscador from "../components/Buscador";
-
 export default function ErrorPage() {
-  const getPath = "http://localhost:3001/get/productos";
-  const delPath = "http://localhost:3001/delete/producto";
-  const updatePath = "http://localhost:3001/update/producto";
-
-  const dataEncabezado = [
-    "ID",
-    "Nombre",
-    "Precio Costo",
-    "Precio Venta",
-    "Stock",
-    "Descripcion",
-    "Fecha de Compra",
-  ];
-
-  return (
-    <Buscador
-      titulo="LISTA DE PRODUCTOS TEST"
-      placeholder="Ingrese el nombre del producto test"
-      encabezado={dataEncabezado}
-      getPath={getPath}
-      updatePath={updatePath}
-      delPath={delPath}
-    />
-  );
+  return <h1>LA PAGINA A LA QUE SE QUIERE ACCEDER NO EXISTE!!!</h1>;
 }
