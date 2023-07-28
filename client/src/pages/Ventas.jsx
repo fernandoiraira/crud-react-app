bd11cfcf52585c56ba411c70d35a2d39578db7f6
fernandoiraira <fmiraira@gmail.com>
2023-07-27 21:21:56 -0300

    Update Ventas.jsx

--- client/src/pages/Ventas.jsx
+++ client/src/pages/Ventas.jsx
@@ -1,7 +1,149 @@
-import React from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Modalreact from "../components/ModalEdProductos";
 
-function Ventas() {
-  return <h1>ESTA ES LA PAGINA DE VENTAS</h1>;
+function Productos() {
+  const [productosLista, setProductos] = useState([]);
+  const [productoBuscado, setBuscado] = useState("");
+  const [editar, setEditar] = useState(false);
+  const [productoEditado, setEditado] = useState({});
+
+  const handleEditar = (elem) => {
+    setEditado(elem);
+    setEditar(true);
+  };
+
+  const handleClose = () => {
+    setEditar(false);
+  };
+
+  const handleChange = (event) => {
+    setBuscado(event.target.value);
+    console.log(productoBuscado);
+  };
+
+  const getProductos = () => {
+    axios.get("http://localhost:3001/get/productos").then((response) => {
+      setProductos(response.data);
+    });
+  };
+
+  const update = (res) => {
+    console.log("PRODUCTO EDITADO A ENVIAR AL SERVER:", res);
+    axios
+      .put("http://localhost:3001/update/producto/", res) // Envía el objeto completo en la solicitud PUT
+      .then(() => {
+        getProductos();
+        alert("updated!!!");
+      });
+  };
+
+  useEffect(() => {
+    getProductos();
+  }, []);
+
+  // Función para filtrar los productos por el nombre buscado
+  const filtrarProductos = () => {
+    return productosLista.filter((producto) =>
+      producto.nombre.toLowerCase().includes(productoBuscado.toLowerCase())
+    );
+  };
+
+  const deleteProd = (id) => {
+    axios.delete("http://localhost:3001/delete/producto/" + id).then(() => {
+      getProductos();
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="container">
+        <Modalreact
+          onOk={(res) => {
+            setEditado(res);
+            update(res);
+            handleClose();
+          }}
+          onClose={handleClose}
+          isOpen={editar}
+          editado={productoEditado}
+        />
+      </div>
+      <div className="card text-center mt-5">
+        <div className="card-header">
+          SELECCIONE LOS PRODUCTOS CORRESPONDIENTES A LA VENTA
+        </div>
+        <div className="card-body">
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">
+                Buscar:
+              </span>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Ingrese el nombre del producto"
+              aria-label="Nombre"
+              aria-describedby="basic-addon1"
+              onChange={handleChange}
+              name="nombre"
+            />
+          </div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Precio Costo</th>
+                <th scope="col">Precio Venta</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Fecha Compra</th>
+                <th scope="col">Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filtrarProductos().map((elem, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{elem.id}</td>
+                    <td>{elem.nombre} </td>
+                    <td>{elem.precio_costo}</td>
+                    <td>{elem.precio_venta} </td>
+                    <td>{elem.stock} </td>
+                    <td>{elem.descripcion} </td>
+                    <td>{elem.fecha_compra} </td>
+                    <td>
+                      <div
+                        className="btn-group"
+                        role="group"
+                        aria-label="Basic example"
+                      >
+                        <button
+                          type="button"
+                          className="btn btn-info"
+                          onClick={() => {
+                            handleEditar(elem);
+                          }}
+                        >
+                          Editar
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="card-footer text-muted">
+          <button className="btn btn-success">Continuar</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Ventas;
+export default Productos;
